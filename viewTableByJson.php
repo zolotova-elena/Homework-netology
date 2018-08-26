097b5299e025b245f2c50ab3ba1b051f60cb6e2c
zolotova-elena <42401456+zolotova-elena@users.noreply.github.com>
2018-08-26 20:15:39 +0300

    Add files via upload

--- viewTableByJson.php
+++ viewTableByJson.php
@@ -19,23 +19,47 @@
 		<td>Тел</td>
 	</tr>
 	<?
-	$f_json = 'info.json';
-	$json = file_get_contents("$f_json");
-	$json = json_decode($json, true);
-	$i = 1;
-	foreach($json as $k => $v ){
-		?>
-		<tr>
-			<td><? echo $i; ?></td>
-			<td><? if ($v['firstName']) {echo $v['firstName']; } else {echo "Отсутствует!";} ?></td>
-			<td><? if ($v['lastName']) {echo $v['lastName'];} else {echo "Отсутствует!";}?></td>
-			<td><? if ($v['address']) {echo $v['address'];} else {echo "Отсутствует!";}?></td>
-			<td><? if ($v['phoneNumber']) {echo $v['phoneNumber'];} else {echo "Отсутствует!";}?></td>
-		</tr>
-		<?
+		$f_json = 'info.json';
+		$json = file_get_contents("$f_json");
+		$json = json_decode($json, true);
+		$i = 1;
+		foreach($json as $k => $v ){
+	?>
+	<tr>
+		<td> 
+			<? 
+				echo $i; 
+			?>		
+		</td>
+		<td>
+			<? 
+				if ($v['firstName']) {echo $v['firstName']; } 
+			 	else {echo "Отсутствует!";} 
+			?>	
+		</td>
+		<td>
+			<? 
+				if ($v['lastName']) {echo $v['lastName'];} 
+				else {echo "Отсутствует!";}
+			?>	
+		</td>
+		<td>
+			<? 
+				if ($v['address']) {echo $v['address'];} 
+				else {echo "Отсутствует!";}
+			?>
+		</td>
+		<td>
+			<? 
+				if ($v['phoneNumber']) {echo $v['phoneNumber'];} 
+				else {echo "Отсутствует!";}
+			?>
+		</td>
+	</tr>
+	
+	<?
 		$i++;
-	}
-
+		}
 
 	?>
 	</table>
