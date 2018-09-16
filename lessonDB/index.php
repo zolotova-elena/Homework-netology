991cef525a4e206e991fd036dd60a845da8bb6d6
zolotova-elena <42401456+zolotova-elena@users.noreply.github.com>
2018-09-16 17:25:05 +0300

    Add files via upload

--- lessonDB/index.php
+++ lessonDB/index.php
@@ -1,3 +1,25 @@
+<?php  
+    header('Content-Type: text/html; charset= utf-8');   
+    $connection = mysqli_connect('127.0.0.1', 'root', '', '4.1'); 
+    $connection->set_charset('utf8');
+    if ($connection == false)
+    {
+        echo "Не удалось подключиться к БД! </br>";
+        echo mysqli_connect_error();
+        exit();
+    } 
+            
+        $isbn = (isset($_POST['isbn']))?$_POST['isbn']:'';
+        $name = (isset($_POST['name']))?$_POST['name']:'';
+        $author = (isset($_POST['author']))?$_POST['author']:'';
+        $isbn = mysqli_real_escape_string($connection, $isbn);
+        $name = mysqli_real_escape_string($connection, $name);
+        $author = mysqli_real_escape_string($connection, $author);
+        //echo $isbn." ".$name." ".$author;
+        $result = mysqli_query($connection, 'SELECT * FROM `books` WHERE LOCATE("'.$isbn.'", isbn) AND LOCATE("'.$name.'", name) AND LOCATE("'.$author.'", author)');
+        
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
     <head>
@@ -6,64 +28,42 @@
     <body>
         <h1>Библиотека успешного человека</h1>
         <form  method="POST">
-            <input type="text" name="isbn" placeholder="isbn" value="<?php echo (isset($_POST['isbn']))?$_POST['isbn']:'';?>">
-            <input type="text" name="name" placeholder="Название" value="<?php echo (isset($_POST['name']))?$_POST['name']:'';?>"> 
-            <input type="text" name="author" placeholder="Автор" value="<?php echo (isset($_POST['author']))?$_POST['author']:'';?>">
+            <input type="text" name="isbn" placeholder="isbn" value="<?php echo $isbn?>">
+            <input type="text" name="name" placeholder="Название" value="<?php echo $name?>"> 
+            <input type="text" name="author" placeholder="Автор" value="<?php echo $author?>">
             <input type="submit" value="Найти">  
-        </form>
-
-        <?php 
-        
-            $connection = mysqli_connect('127.0.0.1', 'root', '', '4.1'); 
-
-            if ($connection == false)
-            {
-                echo "Не удалось подключиться к БД! </br>";
-                echo mysqli_connect_error();
-                exit();
-            } 
-            
-            if(!empty($_POST)) {
-                $isbn = $_POST['isbn'];
-                $name = $_POST['name'];
-                $author = $_POST['author'];
-                //echo $isbn." ".$name." ".$author;
-                $result = mysqli_query($connection, 'SELECT * FROM `books` WHERE LOCATE("'.$isbn.'", isbn) AND LOCATE("'.$name.'", name) AND LOCATE("'.$author.'", author)');
-            } else{
-
-                $result = mysqli_query($connection, 'select * from `books`');
-            }
-            
-            echo '<table border="1">';
-            echo '<thead>';
-            echo '<tr>';
-            echo '<th>id</th>';
-            echo '<th>Название</th>';
-            echo '<th>Автор</th>';
-            echo '<th>Год</th>';
-            echo '<th>isbn</th>';
-            echo '<th>Жанр</th>';
-            echo '</tr>';
-            echo '</thead>';
-            echo '<tbody>';
-
-            while (($record = mysqli_fetch_assoc($result)))
-            { 
-
+            <?
+                echo '<table border="1">';
+                echo '<thead>';
                 echo '<tr>';
-                echo '<td>' . $record['id'] . '</td>';
-                echo '<td>' . $record['name'] . '</td>';
-                echo '<td>' . $record['author'] . '</td>';
-                echo '<td>' . $record['year'] . '</td>';
-                echo '<td>' . $record['isbn'] . '</td>';
-                echo '<td>' . $record['genre'] . '</td>';
+                echo '<th>id</th>';
+                echo '<th>Название</th>';
+                echo '<th>Автор</th>';
+                echo '<th>Год</th>';
+                echo '<th>isbn</th>';
+                echo '<th>Жанр</th>';
                 echo '</tr>';
-            }
+                echo '</thead>';
+                echo '<tbody>';
 
-            echo '</tbody>';
-            echo '</table>';
+                while (($record = mysqli_fetch_assoc($result)))
+                { 
 
-            mysqli_close($connection);
-        ?>
+                    echo '<tr>';
+                    echo '<td>' . $record['id'] . '</td>';
+                    echo '<td>' . $record['name'] . '</td>';
+                    echo '<td>' . $record['author'] . '</td>';
+                    echo '<td>' . $record['year'] . '</td>';
+                    echo '<td>' . $record['isbn'] . '</td>';
+                    echo '<td>' . $record['genre'] . '</td>';
+                    echo '</tr>';
+                }
+
+                echo '</tbody>';
+                echo '</table>';
+
+                mysqli_close($connection);
+            ?>
+        </form>
     </body>
 </html>
