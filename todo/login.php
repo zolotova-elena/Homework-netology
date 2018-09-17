a4939c637b130758119165f62df28708c7016eb2
zolotova-elena <42401456+zolotova-elena@users.noreply.github.com>
2018-09-17 23:51:44 +0300

    Add files via upload

---
+++ todo/login.php
@@ -0,0 +1,101 @@
+<?php
+    //header('Content-Type: text/html; charset= utf-8'); 
+    session_start();
+    function getPDO(){
+        $pdo = new PDO("mysql:host=localhost;dbname=TODO;charset=utf8", "root", "", [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+        //$pdo->set_charset('utf8');
+        return $pdo;
+    }
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'  ) {
+            if (!empty($_POST['login']) && !empty($_POST['pass']) && empty($_POST['newLogin']) && empty($_POST['newPass'])){
+                $login = $_POST['login'];
+                $password = $_POST['pass'];
+                
+                if ( login($login, $password) ){
+                    header('Location: index.php'); 
+                    die;
+                }
+            }
+            if (!empty($_POST['newLogin']) && !empty($_POST['newPass']) && empty($_POST['login']) && empty($_POST['pass'])) {
+                  $login = $_POST['newLogin'];
+                  $password = $_POST['newPass'];
+                 
+                  auth($login, $password);
+            }
+        }
+
+
+        function login($login, $password){
+            $pdo = getPDO();
+            $sql = "SELECT id FROM user WHERE login=".$login." AND password=".$password;
+            $statement = $pdo->prepare($sql);
+            $statement->execute();
+            $empty = $statement->rowCount() === 0;
+            if(!$empty){
+                foreach ($statement as $row) {
+                  $_SESSION['id'] = $row['id'];
+                  return true;
+                }
+            } 
+            else {
+                echo "</br>Неверный пароль или логин!";
+                return false;
+            }
+        }
+
+        function auth($newLogin, $newPassword){
+            $pdo = getPDO();
+            $sql = "SELECT id FROM user WHERE login=$newLogin";
+            $statement = $pdo->prepare($sql);
+            $statement->execute();
+            $empty = $statement->rowCount() === 0;
+            if($empty){
+                $sql1 = "INSERT INTO user(`login`, `password`) VALUES ($newLogin, $newPassword)";
+                    $statement1 = $pdo->prepare($sql1);
+                    $statement1->execute();
+                    echo "Успех";
+                    login($newLogin, $newPassword);
+            } 
+            else {
+                echo "</br>Пользователь с таким логином уже существует!";
+                return false;
+            }
+        }
+
+        
+
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+
+        <div class="container">
+            <form role="form" method="POST">
+                <h2>Если вы пользователь - авторизуйстесь</h2>
+                <input type="text" name="login" class="form-control" placeholder="login" >
+                <br>
+                <br>
+                <input type="text" name="pass" class="form-control" placeholder="Password" >
+                <br>
+                <br>
+
+                <h2>Или зарегистрируйтесь</h2>
+                <input name="newLogin" placeholder="Новый логин"> 
+                <br>
+                <br>
+                <input name="newPass"  placeholder="Новый пароль"> 
+                <br>
+                <br>
+                <button type="submit">Вход/регистрация</button>
+            </form>
+        </div> 
+    </body>
+</html>
